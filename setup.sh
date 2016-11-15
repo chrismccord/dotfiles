445d5155a064de85040e851ec8b01bcadbe8975d
Chris McCord <chris@chrismccord.com>
2016-11-15 18:32:38 -0500

    Add setup

---
+++ setup.sh
@@ -0,0 +1,7 @@
+ln -s .bash .dotfiles/.bash
+ln -s .bash_profile .dotfiles/.bash_profile
+ln -s .bashrc .dotfiles/.bashrc
+ln -s .gitconfig .dotfiles/.gitconfig
+ln -s .tmux.conf .dotfiles/.tmux.conf
+ln -s .zshenv .dotfiles/.zshenv
+ln -s .zshrc .dotfiles/.zshrc
